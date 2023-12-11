c1004ec91174595bba7559a8308d5f09fb02fbf3
EdenEast <edenofest@gmail.com>
2023-12-11 02:22:22 -0500

    feat: add solution for day 07 for 2023

---
+++ rust/aoc_2023/benches/day_07.rs
@@ -0,0 +1,20 @@
+use aoc_2023::day_07::Day07;
+use common::Solution;
+
+fn main() {
+    divan::main();
+}
+
+#[divan::bench]
+fn part_1() {
+    Day07.part_a(divan::black_box(include_str!(
+        "../../../puzzles/2023/07/input"
+    )));
+}
+
+#[divan::bench]
+fn part_2() {
+    Day07.part_b(divan::black_box(include_str!(
+        "../../../puzzles/2023/07/input"
+    )));
+}
